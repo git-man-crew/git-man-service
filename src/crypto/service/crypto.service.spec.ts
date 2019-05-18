e57b7b487c362060c40898d58c82d5b482e3035e
Leon Jäkel <info@leon-jaekel.com>
2019-05-18 10:10:25 +0200

    Adds tests for crypto service

--- src/crypto/service/crypto.service.spec.ts
+++ src/crypto/service/crypto.service.spec.ts
@@ -29,4 +29,20 @@ describe('CryptoService', () => {
     const decryptedData = service.decryptText(encryptedData);
     expect(decryptedData).toBe(testData);
   });
+
+  it('should encrypt test-data without environment', async () => {
+    delete process.env.CRYPTO_SECRET_LENGTH;
+    delete process.env.NODE_ENV;
+    const moduleWithoutEnv: TestingModule = await Test.createTestingModule({
+      imports: [CryptoModule],
+    }).compile();
+
+    const cryptoService = moduleWithoutEnv.get<CryptoService>(CryptoService);
+    const testData: string = 'test-data';
+    const encryptedData = cryptoService.encryptText(testData);
+    const decryptedData = cryptoService.decryptText(encryptedData);
+    process.env.CRYPTO_SECRET_LENGTH = '28';
+    process.env.NODE_ENV = 'test';
+    expect(decryptedData).toBe(testData);
+  });
 });
